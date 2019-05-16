b82f8cd53d41b7c1706bf773fb11c3ec1898c3b2
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-16 11:16:30 +0200

    yaaaaaaaa

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,15 +1,20 @@
 class FlatsController < ApplicationController
-
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
+    @flats = Flat.all
   end
 
   def show
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    flat = Flat.new(flat_params)
+    flat.save
+    redirect_to flat_path(@flat.id)
   end
 
   def edit
@@ -19,6 +24,16 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 
 end
